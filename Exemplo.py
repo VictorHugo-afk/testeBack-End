f1fd94364c11f1ae016a39334eb918c71cd3fa47
Victor Hugo Ribeiro <ribeiro.santos.victor3007@escola.pr.gov.br>
2026-02-19 08:16:41 -0300

    Update Exemplo.py
    19/02/26

--- Exemplo.py
+++ Exemplo.py
@@ -2,3 +2,7 @@ print ("Hello World")
 import this
 print (7+4)
 print ('7'+'4')
+nome=input('Qual  o seu nome')
+idade=input('Qual é a sua idade')
+peso=input('Qual é o seu peso')
+print(nome,idade,peso)
