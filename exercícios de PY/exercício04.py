66b217823e6fbd5c15d8b56f1e1d758b3f02d8d2
Victor Hugo Ribeiro <ribeiro.santos.victor3007@escola.pr.gov.br>
2026-03-03 09:50:16 -0300

    03/03/26

--- exercícios de PY/exercício04.py
+++ exercícios de PY/exercício04.py
@@ -3,3 +3,7 @@
 #float é para numeros decimais
 #booleano é para armazenar  valores de verdadeiro e falso
 #str armazena texto
+
+num01 = int  (input ('Digite o primeiro número'))
+num02 = int  (input ('Digite o segundo número')) 
+print (num01+num02)
---
+++ exercícios de PY/exercício05.py
@@ -0,0 +1,4 @@
+num01 = int(input("digite seu primeiro numero"))
+num02 = int(input('digites seu segundo numero'))
+soma = num01 + num02
+print('A soma de {} + {}, é {}' .format (num01, num02, soma))
