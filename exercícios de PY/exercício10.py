dd86823420daaf3ea5b35f55e45a648f495829c1
Victor Hugo Ribeiro <ribeiro.santos.victor3007@escola.pr.gov.br>
2026-03-11 10:57:23 -0300

    11/03/26

---
+++ exercícios de PY/exercício10.py
@@ -0,0 +1,4 @@
+n1 = float (input ('Quantos metros tem?'))
+cm = n1*100
+ml = n1*1000
+print ('O centímetros é {} e o milímetros é {}' .format( cm, ml))
---
+++ exercícios de PY/exercício11.py
@@ -0,0 +1,12 @@
+n1 = int (input ('Escolha um número da tabuada'))
+n2 = n1*1
+n3 = n1*2
+n4 = n1*3
+n5 = n1*4
+n6 = n1*5
+n6 = n1*6
+n7 = n1*7
+n8 = n1*8
+n9 = n1*9
+n10 = n1*10
+print ('O resultado da multiplacação do seu número é {} seu número multiplicado por 2 é {} seu número multiplicado por 3 é {}seu número multiplicado por 4 é {} seu número multiplicado por 5 é {} seu número multiplicado por 6 é {} seu número multiplicado por 7 é {} seu número multiplicado por 8 é {} seu número multiplicado por 9 é {} seu número multiplicado por 10 é {}' .format(n2, n3, n4, n5, n6, n7, n8, n9, n10))
