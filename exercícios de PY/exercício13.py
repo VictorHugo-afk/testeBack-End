a02309dac8560f5aacb736d62d44713cb81a9104
Victor Hugo Ribeiro <ribeiro.santos.victor3007@escola.pr.gov.br>
2026-03-17 12:27:50 -0300

    17/03/26

---
+++ exercícios de PY/exercício13.py
@@ -0,0 +1,6 @@
+altura = float (input(' Qual a sua altura: ?'))
+largura = float (input(' Qual é sua largura: ?'))
+area = altura*largura
+tinta = area/2
+print ('A sua altura é {}, sua area é de {}, e sua tinta final de quantidade de litros é de 1.79{}' .format(altura, largura, area, tinta))
+area = tinta 
---
+++ exercícios de PY/exercício15.py
@@ -0,0 +1,3 @@
+salario = float (input('Seu salario atual é de: '))
+aumento = salario + (salario *15 / 100)
+print ('O salario que custava {:.2f}, teve um aumento de 15% é agora vai passar a ser R$ {:.2f}' .format(salario, aumento))
