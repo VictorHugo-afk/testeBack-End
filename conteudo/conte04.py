2df23d8463fea19603abd8fc5ed1c90ed3b5b6a4
Victor Hugo Ribeiro <ribeiro.santos.victor3007@escola.pr.gov.br>
2026-03-23 08:10:24 -0300

    23/03/26

---
+++ conteudo/conte04.py
@@ -0,0 +1,8 @@
+# Módulos em Python
+# Para importar bibliotecas e funcionalidades em Python utiliza-se o comando Import seguido do nome biblioteca ou módulo que ira ser carregado nesse programa. 
+# Para importar coisas específicas de uma biblioteca utiliza-se o comando from seguido do nome da biblioteca seguido da palavra import seguido do item que deseja importar.
+
+import math
+num = int (input('Digite um número'))
+raiz = math.sqrt(num)
+print('A raiz de {} é igual a {}' .format(num, raiz))
---
+++ conteudo/conte06.py
@@ -0,0 +1,1 @@
+# Biblioteca math2
