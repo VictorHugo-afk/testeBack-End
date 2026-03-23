5713ccb913876116e1399b369e04fc09a4ddf33e
Victor Hugo Ribeiro <ribeiro.santos.victor3007@escola.pr.gov.br>
2026-03-23 12:22:24 -0300

    23/03/26

--- conteudo/conte04.py
+++ conteudo/conte04.py
@@ -2,7 +2,14 @@
 # Para importar bibliotecas e funcionalidades em Python utiliza-se o comando Import seguido do nome biblioteca ou módulo que ira ser carregado nesse programa. 
 # Para importar coisas específicas de uma biblioteca utiliza-se o comando from seguido do nome da biblioteca seguido da palavra import seguido do item que deseja importar.
 
-import math
-num = int (input('Digite um número'))
-raiz = math.sqrt(num)
-print('A raiz de {} é igual a {}' .format(num, raiz))
+# import math
+# num = int (input('Digite um número'))
+# raiz = math.sqrt(num)
+# print('A raiz de {} é igual a {}' .format(num, math.ceil (raiz)))
+# print('A raiz de {} é igual a {}' .format(num, math.floor (raiz)))
+
+# Importando funcionalidades específicas
+
+from math import sqrt, floor
+num = int (input)
+raiz = sqrt (num)
