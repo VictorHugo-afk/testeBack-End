078d0fe03caa13a4dca11ea4848c7d0bec44bb38
Victor Hugo Ribeiro <ribeiro.santos.victor3007@escola.pr.gov.br>
2026-03-30 12:26:52 -0300

    30/03/26

---
+++ exercícios de PY/exercício19.py
@@ -0,0 +1,4 @@
+catop = float (input('Cateto Oposto: '))
+catad = float (input('Cateto Adjacente: '))
+hi = (catop**2+catad**2)**0.5
+print ('A hipotenusa vai medir {}' .format(hi))
---
+++ exercícios de PY/exercício20.py
@@ -0,0 +1,8 @@
+import math
+ang = float (input('Qual é o ângulo? '))
+cos = math.acosh ()
+sen = math.asinh ()
+tang = math.atanh ()
+print(cos)
+print(sen)
+print(tang)
