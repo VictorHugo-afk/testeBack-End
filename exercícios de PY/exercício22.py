ad2a36750a0b4ed992f2c017958257602836fb65
Victor Hugo Ribeiro <ribeiro.santos.victor3007@escola.pr.gov.br>
2026-04-01 09:06:25 -0300

    01/04/26

--- exercícios de PY/exercício22.py
+++ exercícios de PY/exercício22.py
@@ -1,3 +1,8 @@
-import a
-GrupoA = str (input('Grupo A: '))
-GrupoB = str (input('Grubo B: '))
+from random import shuffle
+Aluno1 = str (input('Primeiro Aluno: '))
+Aluno2 = str (input('Segundo Aluno: '))
+Aluno3 = str (input('Terceiro Aluno: '))
+Aluno4 = str (input('Quarto Aluno: '))
+lista = [Aluno1, Aluno2, Aluno3, Aluno4]
+shuffle (lista)
+print ('A ordem de apresentação será {}' .format(lista))
