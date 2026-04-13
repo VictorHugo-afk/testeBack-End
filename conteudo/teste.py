5cd89435e27fdd8bc78a53979427718052ad3540
Victor Hugo <ribeiro.santos.victor3007@escola.pr.gov.br>
2026-04-13 08:27:07 -0300

    13/0426

---
+++ conteudo/teste.py
@@ -0,0 +1,23 @@
+#num = int(input('Digite um número entre 0 e 9999: '))
+#unidade = num % 10
+#centena = (num // 100) % 10
+#print(f'Unidade: {unidade}')
+#print(f'Centena: {centena}')
+
+#nome_completo = input("Digite o nome completo: ").strip()
+#nomes = nome_completo.split()
+#if len(nomes) > 1:
+#    segundo_nome = nomes[1]
+#    quantidade_letras = len(segundo_nome)
+#    print(f"O segundo nome é '{segundo_nome}' e ele tem {quantidade_letras} letras.")
+#else:
+#    print("Você digitou apenas um nome. Não foi possível encontrar o segundo.")
+
+#frase = "estou fazendo uma prova de backend"
+#frase_modificada = frase.replace(" ", "*")
+#print(frase_modificada)
+
+frase = "A lógica é o princípio da sabedoria"
+resultado_split = frase.split()
+print(resultado_split[2])
+
