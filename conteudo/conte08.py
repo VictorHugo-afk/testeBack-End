f51bb3f7c87102f3501ce5c032ea89928c172d6a
Victor Hugo <ribeiro.santos.victor3007@escola.pr.gov.br>
2026-04-28 09:48:11 -0300

    28/04/26

--- conteudo/conte08.py
+++ conteudo/conte08.py
@@ -1,6 +1,28 @@
-tempo = int(input("Quantos anos tem seu carro?"))
-if  tempo<=3:
-    print('carro novo')
+#tempo = int(input("Quantos anos tem seu carro?"))
+#if  tempo<=3:
+#    print('carro novo')
+#else:
+#    print('carro velho')
+#print('--fim--')
+
+#tempo = int(input("Quantos anos tem seu carro?"))
+#print('carro novo' if tempo<=3 else'carro velho')
+#print('--fim--')
+
+#nome = str(input('Qual é o seu nome?'))
+#if nome == 'Victor':
+# print('Que nome lindo você tem!')
+#else:
+# print('seu nome é tão normal')
+#print('Bom dia {}!'.format(nome))
+
+n1 = float(input("Digite a primeira nota: "))
+n2 = float(input("Digite a segunda nota: "))
+m = (n1 + n2) / 2
+
+print("A sua média foi {:.1f}".format(m))
+
+if m >= 6.0:
+    print("A sua média foi boa! Parabéns")
 else:
-    print('carro velho')
-print('--fim--')
+    print("Sua média foi ruim! Estude mais")
