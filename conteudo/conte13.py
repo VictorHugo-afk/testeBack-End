82043521c066660eada1e53f29c3993d2a2db9a7
Victor Hugo <ribeiro.santos.victor3007@escola.pr.gov.br>
2026-08-11 09:03:44 -0300

    11/08/26

--- conteudo/conte13.py
+++ conteudo/conte13.py
@@ -2,11 +2,37 @@
 #  Relembrando do primeiro trimestre, toda vez que um variável simples é declarada um espaço na memória e criado no computador. Onde valores são guardados pelo operador de =. Em variáveis simples se eu quiser guardar mais um valor dentro do espaço criado na memória o que estava armazenado anteriormente é excluido e o novo valor é armazenado. Para resolvermos esse problema podemos criar mais espaços na memória em uma mesma variável, e uma dessas formas e com o uso de tuplas. Os elementos de uma tupla pode ser selecionados através de seus índices que inicia-se no valor 0.
 # Dentro das tuplas podemos usar métodos de fatiamento, de length e o uso das variáveis de repetição. As tuplas são imutáveis em Python ou seja os valores armazenados nessas variáveis não podem ser alterados após definidos. As tuplas são definidas entre parênteses
 
-lanche = ('Hamburger', 'Suco', 'Pizza', 'Pudim')
-print(lanche)
-print(lanche [1])
-print(lanche [-2])
-print(lanche [1:3])
-print(lanche [2:]) #Ele pega o segundo elemento e começa a contar do terceiro elemento
-print(lanche [:2])
-print(lanche [-2:])
+# lanche = ('Hamburger', 'Suco', 'Pizza', 'Pudim')
+# print(lanche)
+# print(lanche [1])
+# print(lanche [-2])
+# print(lanche [1:3])
+# print(lanche [2:]) #Ele pega o segundo elemento e começa a contar do terceiro elemento
+# print(lanche [:2])
+# print(lanche [-2:])
+
+# lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim')
+# for comida in lanche:
+#     print(f'Eu vou comer {comida}')
+# print('comi pra caramba')
+# print(len(lanche))
+# for cont in range(0, len(lanche)):
+#     print(f'Eu vou comer {lanche[cont]} na posição {cont}')  # outra forma de fazer
+
+# lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim')
+# print(sorted (lanche))
+# a = (2,5,4)
+# b = (5,8,1,2)
+# print (a)
+# print(b)
+# c = a+b
+# print(c)
+# print (len (c))
+# print (c.count (5))
+# print (c.index (8))
+# print (c.index (5,2))
+
+pessoa = ('Victor H.', 18, 'M')
+print (pessoa)
+del pessoa 
+print (pessoa)
