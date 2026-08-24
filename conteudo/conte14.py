4d027f2f38ab4f01075f0b7d400dcc8f435ff9b8
Victor Hugo <ribeiro.santos.victor3007@escola.pr.gov.br>
2026-08-24 08:28:20 -0300

    24/08/26

--- conteudo/conte14.py
+++ conteudo/conte14.py
@@ -1,7 +1,26 @@
-num = [2, 5, 9, 1]
-num [2] = 3
-num.append (7)
-num.sort (reverse=True)
-num.insert (2,2)
-num.remove (2)
-print(num)
+# num = [2,5,9,1]
+# num[2] = 3 #pode modificar a lista
+# #num [4] 7 #vai dar erro porque não tem esse índice na lista
+# num.append(7)
+# num.sort()
+# num.sort(reverse=True)
+# num.insert(2,0) #adiciona na posição 2 o valor de zero empurrando os outros elementos.
+# num.insert(2,2) #adiciona na posição 2 o valor de zero empurrando os outros elementos.
+# num.remove(2) # remove a primeira ocorrência
+# num.remove(3) #da erro pois não exite o númro 4
+# if 4 in num:
+#  num.remove(4) #maneira certa de remover
+# else:
+#  print('não achei o número 4')
+# num.pop(2)#elimina o elemento 2
+# print(num)
+# print(f'Essa lista tem {len(num)} elementos')
+
+valores = []
+valores.append (5)
+valores.append (9)
+valores.append (4)
+print (valores)
+
+for c,v in enumerate (valores):
+ print (f'na posição {c} eu achei o valor {v}', end='')
