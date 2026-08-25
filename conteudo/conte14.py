d657a6f8525bc1caa8edd80d4ae846ac7cccc654
Victor Hugo <ribeiro.santos.victor3007@escola.pr.gov.br>
2026-08-25 09:55:47 -0300

    25/08/26

--- conteudo/conte14.py
+++ conteudo/conte14.py
@@ -16,11 +16,27 @@
 # print(num)
 # print(f'Essa lista tem {len(num)} elementos')
 
-valores = []
-valores.append (5)
-valores.append (9)
-valores.append (4)
-print (valores)
+# valores = []
+# valores.append (5)
+# valores.append (9)
+# valores.append (4)
+# print (valores)
 
-for c,v in enumerate (valores):
- print (f'na posição {c} eu achei o valor {v}', end='')
+# for c,v in enumerate (valores):
+#  print (f'na posição {c} eu achei o valor {v}', end='')
+
+# valores = []
+# for cont in range (0,5):
+#     valores.append (int(input('Digite um valor ')))
+# for c,v in enumerate (valores):
+#     print(f'Eu achei na posição {c} o valor de {v}')
+
+# a = [2,3,4,7]
+# #b = [4,5,6,7]
+# #b = a
+# b = a [:]
+# b [2] = 8
+
+
+# print (f'Lista A {a}')
+# print (f'Lista B {b}')
